669726d2246ac46fcbbbc37a56933ce161f1d59b
daniel <squaredfreezer@gmail.com>
2024-04-12 13:09:59 -0400

    add dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import LoginPage from './Pages/LoginPage';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="*" element={<Home/>} />
         <Route path ="/login" element={<LoginPage/>} />
+        <Route path = "/dashboard" element={<Dashboard/>} />
       </Routes>
     </Router>
   );
